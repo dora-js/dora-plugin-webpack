256d5c896f26a4417cd9eec49c25addad955258b
xuanji.jw <xuanji.jw@taobao.com>
2016-08-08 17:47:55 +0800

    enhance publicPath

--- test/index-test.js
+++ test/index-test.js
@@ -4,21 +4,21 @@ import expect from 'expect';
 import { join } from 'path';
 
 describe('index', () => {
-
-  const port = 12347;
   const oldCwd = process.cwd();
-  const cwd = join(__dirname, './fixtures/app');
+  const cwdWithoutConfig = join(__dirname, './fixtures/app_without_config');
+  const cwdWithConfig = join(__dirname, './fixtures/app_with_config');
 
-  describe('publicPath', () => {
+  describe('publicPath-without-config', () => {
+    const portWithoutConfig = 12347;
 
     before(done => {
-      process.chdir(cwd);
+      process.chdir(cwdWithoutConfig);
       dora({
-        port,
+        port: portWithoutConfig,
         plugins: [
           '../../../src/index?{"publicPath": "/foo/", "verbose":true}'
         ],
-        cwd: cwd,
+        cwd: cwdWithoutConfig,
         verbose: true,
       });
       setTimeout(done, 1000);
@@ -29,7 +29,7 @@ describe('index', () => {
     });
 
     it('/foo/index.js', (done) => {
-      request(`http://localhost:${port}/foo/index.js`, (err, res, body) => {
+      request(`http://localhost:${portWithoutConfig}/foo/index.js`, (err, res, body) => {
         expect(res.statusCode).toEqual(200);
         expect(body.indexOf('webpackJsonp') > -1).toExist();
         done();
@@ -37,7 +37,45 @@ describe('index', () => {
     });
 
     it('/index.js', (done) => {
-      request(`http://localhost:${port}/index.js`, (err, res, body) => {
+      request(`http://localhost:${portWithoutConfig}/index.js`, (err, res, body) => {
+        expect(res.statusCode).toEqual(200);
+        expect(body.indexOf('webpackJsonp') > -1).toNotExist();
+        done();
+      })
+    });
+
+  });
+
+  describe('publicPath-with-config', () => {
+    const portWithConfig = 12348;
+
+    before(done => {
+      process.chdir(cwdWithConfig);
+      dora({
+        port: portWithConfig,
+        plugins: [
+          '../../../src/index?{"verbose":true}'
+        ],
+        cwd: cwdWithConfig,
+        verbose: true,
+      });
+      setTimeout(done, 1000);
+    });
+
+    after(() => {
+      process.chdir(oldCwd);
+    });
+
+    it('/coo/index.js', (done) => {
+      request(`http://localhost:${portWithConfig}/coo/index.js`, (err, res, body) => {
+        expect(res.statusCode).toEqual(200);
+        expect(body.indexOf('webpackJsonp') > -1).toExist();
+        done();
+      });
+    });
+
+    it('/index.js', (done) => {
+      request(`http://localhost:${portWithConfig}/index.js`, (err, res, body) => {
         expect(res.statusCode).toEqual(200);
         expect(body.indexOf('webpackJsonp') > -1).toNotExist();
         done();
